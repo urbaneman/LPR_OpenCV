589ea39b2ef2b694e80a8637f80123c3bd2f08af
urbaneaman <15955788606@163.com>
2018-01-28 22:19:02 +0800

    add function resize() change function pre_treatment()

--- src/main/java/com/wangbin/lpr_opencv/ImageProcess.java
+++ src/main/java/com/wangbin/lpr_opencv/ImageProcess.java
@@ -44,31 +44,55 @@ public class ImageProcess {
         return licenseplate;
     }
 
-    public void Pre_treatment(Mat src){
-        dst = new Mat();
-        if(src.channels() == 3)
-            Imgproc.cvtColor(src,dst,Imgproc.COLOR_RGB2GRAY);
-        else if(src.channels() == 4)
-            Imgproc.cvtColor(src,dst,Imgproc.COLOR_RGBA2GRAY);
-
-        //TODO：适配各种图片，resize合适的大小
-        if(dst.cols() >= 1500 && dst.rows() >= 2000) {
-            Size smallsize = new Size(1000, 1000 * dst.rows() / dst.cols());
-            Imgproc.resize(dst, dst, smallsize, 0, 0, Imgproc.INTER_LINEAR);
-        }
+    public void Pre_treatment(){
+        //调整图像大小
+        resize(src);
+        Mat img_pro = new Mat();
+        //高斯模糊
         Size gaussiansize = new Size(3, 3);
-        Imgproc.GaussianBlur(dst,dst, gaussiansize,0);
-        Imgproc.Sobel(dst,dst,CvType.CV_8UC1,1,0);
+        Imgproc.GaussianBlur(src,img_pro, gaussiansize,0);
+
+        //灰度化
+        if(src.channels() == 3)
+            Imgproc.cvtColor(img_pro,img_pro,Imgproc.COLOR_RGB2GRAY);
+        else if(src.channels() == 4)
+            Imgproc.cvtColor(img_pro,img_pro,Imgproc.COLOR_RGBA2GRAY);
+
+        //Sobel 水平边缘提取
+        Imgproc.Sobel(img_pro, img_pro, CvType.CV_8UC1, 1, 0);
+
+        //二值化
+        Imgproc.threshold(img_pro, img_pro, 0, 255, Imgproc.THRESH_OTSU);
 
         //TODO：寻找开闭运算的最佳内核以及最佳处理顺序
-        Size ksize_open = new Size(3, 3);
-        Size ksize_close = new Size(7, 7);
+        Size ksize_open = new Size(9, 9);
+        Size ksize_close = new Size(19, 7);
         Point anchor = new Point(-1,-1);
         Mat element_open = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT,ksize_open);
         Mat element_close = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT,ksize_close);
-        Imgproc.morphologyEx(dst,dst,Imgproc.MORPH_OPEN,element_open,anchor,1);
-        Imgproc.morphologyEx(dst,dst,Imgproc.MORPH_CLOSE,element_close,anchor,2);
-        this.dst = dst;
+        Imgproc.morphologyEx(img_pro,img_pro,Imgproc.MORPH_CLOSE,element_close,anchor,1);
+        Imgproc.morphologyEx(img_pro,img_pro,Imgproc.MORPH_OPEN,element_open,anchor,1);
+        this.dst = img_pro;
+    }
+
+    //调整输入图像的大小
+    //针对手机虚假分辨率，将调整从手机直接拍摄获得的车牌图片
+    //一般手机采集图像尺寸比例为4:3; 16:9和1:1三种
+    //所以根据这三种尺寸的图像进行缩放至宽度为1080
+    public void resize(Mat input){
+        //如果宽度大于1080
+        if(input.cols()>1080){
+            Mat dst = new Mat();
+            double rows = input.rows();
+            double ratio = rows / input.cols();
+            if(ratio>=1.77 && ratio<=1.78 || ratio == 1.0 || ratio>= 1.33 && ratio <= 1.34){
+                Size smallsize = new Size(1080, 1080 * dst.rows() / dst.cols());
+                Imgproc.resize(input, dst, smallsize, 0, 0, Imgproc.INTER_LINEAR);
+                this.setSrc(dst);
+            }
+        }else {
+            return;
+        }
     }
 
 }
