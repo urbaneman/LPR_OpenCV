d50acd75960e1c1e8e7ad206d79e382f58a9b9c1
urbaneaman <15955788606@163.com>
2018-01-30 23:57:07 +0800

    change same value

--- src/main/java/com/wangbin/lpr_opencv/ValueOfImgproc.java
+++ src/main/java/com/wangbin/lpr_opencv/ValueOfImgproc.java
@@ -17,7 +17,7 @@ public final class ValueOfImgproc {
 
     public static final int inputImg_maxWidth = 1080, inputImg_maxHeight = 1080;
 
-    public static final int resizeImg_width = 540, resizeImg_height = 1080;
+    public static final int resizeImg_width = 720, resizeImg_height = 1080;
 
     public static final double ratio_img_4_3_min = 1.33, ratio_img_4_3_max = 1.34,
                                ratio_img_16_9_min = 1.77,ratio_img_16_9_max = 1.78;
@@ -27,9 +27,10 @@ public final class ValueOfImgproc {
 
     //public static final double contour_minAngle = -Math.PI/6, contour_maxAngle = Math.PI/6;
 
-    public static final double contour_minAngle = -30, contour_maxAngle = 30;
+    public static final double contour_LayDown_minAngle = -90, contour_LayDown_maxAngle = -60,
+                                contour_LayUp_minAngle = -30, contour_LayUp_maxAngle = 0;
 
-    public static final double contour_minSizeRatio_small = 3.14, contour_maxSizeRatio_samll = 10.0,
+    public static final double contour_minSizeRatio_small = 3.14, contour_maxSizeRatio_samll = 6.0,
                                 contour_minSizeRatio_big = 2.0,contour_maxSizeRatio_big = 3.0;
 
 }
